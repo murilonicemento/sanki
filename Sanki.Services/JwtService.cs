2d86e3ee4b649a2b9b11cc79e574aa6873a67631
murilonicemento <murilo_nascimento22@outlook.com>
2025-01-26 19:18:37 -0300

    feat: create jwt service

---
+++ Sanki.Services/JwtService.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Sanki.Entities;
+using Sanki.Services.Contracts;
+using Sanki.Services.Contracts.DTO;
+
+namespace Sanki.Services;
+
+public class JwtService : IJwtService
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public AuthResponseDTO GenerateJwt(User user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ?? string.Empty));
+        var expiration =
+            DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:EXPIRATION_MINUTES"] ?? string.Empty));
+        var claims = new Claim[]
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Iat, DateTime.Now.ToString(CultureInfo.InvariantCulture)),
+            new(ClaimTypes.Name, user.FirstName),
+            new(ClaimTypes.NameIdentifier, user.Email),
+            new(ClaimTypes.Email, user.Email)
+        };
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenGenerator = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: expiration,
+            signingCredentials: signingCredentials
+        );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.WriteToken(tokenGenerator);
+
+        return new AuthResponseDTO
+        {
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email,
+            Token = token,
+            RefreshToken = GenerateRefreshToken(),
+            RefreshTokenExpiration = expiration
+        };
+    }
+
+    private string GenerateRefreshToken()
+    {
+        var bytes = new byte[64];
+        var randomNumberGenerator = RandomNumberGenerator.Create();
+
+        randomNumberGenerator.GetBytes(bytes);
+
+        return Convert.ToBase64String(bytes);
+    }
+}
