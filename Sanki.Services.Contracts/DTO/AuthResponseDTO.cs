5636b735300fe240b8b705be3b029efa2f8d24f5
murilonicemento <murilo_nascimento22@outlook.com>
2025-01-26 19:17:01 -0300

    feat: create register feature dtos

---
+++ Sanki.Services.Contracts/DTO/AuthResponseDTO.cs
@@ -0,0 +1,16 @@
+namespace Sanki.Services.Contracts.DTO;
+
+public class AuthResponseDTO
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string? Token { get; set; }
+
+    public string? RefreshToken { get; set; }
+
+    public DateTime? RefreshTokenExpiration { get; set; }
+}
---
+++ Sanki.Services.Contracts/DTO/RegisterUserDTO.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Sanki.Entities;
+
+namespace Sanki.Services.Contracts.DTO;
+
+public class RegisterUserDTO
+{
+    [Required(ErrorMessage = "First name can't be blank.")]
+    [MaxLength(50, ErrorMessage = "First name max length is 50 characters.")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name can't be blank.")]
+    [MaxLength(50, ErrorMessage = "Last name max length is 50 characters.")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "First name can't be blank.")]
+    [EmailAddress(ErrorMessage = "Email is not valid.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "First name can't be blank.")]
+    [MaxLength(255, ErrorMessage = "First name max length is 50 characters.")]
+    public string Password { get; set; }
+}
