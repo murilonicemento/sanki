47bd7508588433ec6a6c41ef350e2befbc9a4718
murilonicemento <murilo_nascimento22@outlook.com>
2025-01-26 19:14:20 -0300

    feat: create application db context

---
+++ Sanki.Persistence/SankiContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sanki.Entities;
+
+namespace Sanki.Persistence;
+
+public class SankiContext : DbContext
+{
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Resume> Resumes { get; set; }
+
+    public virtual DbSet<Flashcard> Flashcards { get; set; }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    public SankiContext()
+    {
+    }
+
+    public SankiContext(DbContextOptions<SankiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresExtension("uuid-ossp");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SankiContext).Assembly);
+    }
+}
