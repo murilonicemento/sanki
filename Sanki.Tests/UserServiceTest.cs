6410bb1eb0313b22a8afe317ab5b1e4743aa8acf
murilonicemento <murilo_nascimento22@outlook.com>
2025-01-27 23:27:59 -0300

    test: create test setup

---
+++ Sanki.Tests/UserServiceTest.cs
@@ -0,0 +1,13 @@
+namespace Sanki.Tests;
+
+public class UserServiceTest
+{
+    public UserServiceTest()
+    {
+    }
+
+    [Fact]
+    public async Task Register_PersonAlreadyExist_ToBeInvalidOperationException()
+    {
+    }
+}
