153492ae7f15fd444243b876ea0eab61f795f55f
murilonicemento <murilo_nascimento22@outlook.com>
2025-01-30 22:24:01 -0300

    feat: create custom equals method

--- Sanki.Services.Contracts/DTO/AuthResponseDTO.cs
+++ Sanki.Services.Contracts/DTO/AuthResponseDTO.cs
@@ -13,4 +13,20 @@ public class AuthResponseDTO
     public string RefreshToken { get; set; }
 
     public DateTime RefreshTokenExpiration { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (obj.GetType() != typeof(AuthResponseDTO)) return false;
+
+        AuthResponseDTO authResponseDto = (AuthResponseDTO)obj;
+
+        return FirstName == authResponseDto.FirstName && LastName == authResponseDto.LastName &&
+               Email == authResponseDto.Email;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FirstName, LastName, Email);
+    }
 }
