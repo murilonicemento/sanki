84e4c2829051a5cc23520d7092fdcb24e9a19c45
murilonicemento <murilo_nascimento22@outlook.com>
2025-01-31 00:06:35 -0300

    feat: create login response dto

---
+++ Sanki.Services.Contracts/DTO/LoginUserResponseDTO.cs
@@ -0,0 +1,11 @@
+namespace Sanki.Services.Contracts.DTO;
+
+public class LoginUserResponseDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiration { get; set; }
+}
