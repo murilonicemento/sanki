8d130a008998055c9f321aa57032e05aade42c2d
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-01 17:46:47 -0300

    feat: create migration to add nullable properties

---
+++ Sanki.Persistence/Migrations/20250201204303_MakePropertieNullable.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sanki.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePropertieNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    first_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    salt = table.Column<byte[]>(type: "bytea", nullable: false),
+                    refresh_token = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    refresh_token_expiration = table.Column<DateTime>(type: "timestamp with time zone", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "resumes",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("resumes_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "resumes_fk3",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "flashcards",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    question = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    response = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    resume_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("flashcards_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "flashcards_fk4",
+                        column: x => x.resume_id,
+                        principalTable: "resumes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reviews",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    review_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    flashcard_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("reviews_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "reviews_fk2",
+                        column: x => x.flashcard_id,
+                        principalTable: "flashcards",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_flashcards_resume_id",
+                table: "flashcards",
+                column: "resume_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_resumes_user_id",
+                table: "resumes",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_reviews_flashcard_id",
+                table: "reviews",
+                column: "flashcard_id");
+
+            migrationBuilder.CreateIndex(
+                name: "users_email_key",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reviews");
+
+            migrationBuilder.DropTable(
+                name: "flashcards");
+
+            migrationBuilder.DropTable(
+                name: "resumes");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
