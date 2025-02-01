374ab5604b400861bb00d188479774a15b7519f6
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-01 18:43:07 -0300

    fix: change error messages

--- Sanki.Services.Contracts/DTO/RegisterUserRequestDTO.cs
+++ Sanki.Services.Contracts/DTO/RegisterUserRequestDTO.cs
@@ -13,11 +13,11 @@ public class RegisterUserRequestDTO
     [MaxLength(50, ErrorMessage = "Last name max length is 50 characters.")]
     public string LastName { get; set; }
 
-    [Required(ErrorMessage = "First name can't be blank.")]
+    [Required(ErrorMessage = "Email can't be blank.")]
     [EmailAddress(ErrorMessage = "Email is not valid.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "First name can't be blank.")]
+    [Required(ErrorMessage = "Password can't be blank.")]
     [MinLength(8, ErrorMessage = "Password to short.")]
     [MaxLength(255, ErrorMessage = "First name max length is 255 characters.")]
     public string Password { get; set; }
