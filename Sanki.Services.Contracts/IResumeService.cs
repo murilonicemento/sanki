e8687b567705d0f225d2314f24fd8222ce95fb63
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-02 17:54:45 -0300

    feat: create interface to resume service

---
+++ Sanki.Services.Contracts/IResumeService.cs
@@ -0,0 +1,9 @@
+using Sanki.Services.Contracts.DTO;
+
+namespace Sanki.Services.Contracts;
+
+public interface IResumeService
+{
+    public Task<List<ResumeDTO>> GetResumesByUserAsync(string token);
+    public Task AddResumeAsync(AddResumeRequestDTO addResumeRequestDto);
+}
