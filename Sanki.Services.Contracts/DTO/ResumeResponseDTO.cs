f7bd697c650c77674145fb8156c1bdeeaa168f66
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-03 22:26:21 -0300

    refactor: rename resume dto

--- Sanki.Services.Contracts/DTO/ResumeResponseDTO.cs
+++ Sanki.Services.Contracts/DTO/ResumeResponseDTO.cs
@@ -2,6 +2,7 @@ namespace Sanki.Services.Contracts.DTO;
 
 public class ResumeResponseDTO
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string? Content { get; set; }
 }
