c5dfb20541df7a89eaba099314d16b5eb835dc45
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-03 22:26:36 -0300

    refactor: rename resume dto

--- Sanki.Services.Contracts/IResumeService.cs
+++ Sanki.Services.Contracts/IResumeService.cs
@@ -5,6 +5,6 @@ namespace Sanki.Services.Contracts;
 public interface IResumeService
 {
     public Task<List<ResumeResponseDTO>> GetResumesByUserAsync(string token);
-    public Task AddResumeAsync(ResumeRequestDTO resumeRequestDto, string token);
-    public Task<ResumeResponseDTO> UpdateResumeAsync(ResumeRequestDTO resumeRequestDto, string token);
+    public Task AddResumeAsync(AddResumeRequestDTO addResumeRequestDto, string token);
+    public Task<ResumeResponseDTO> UpdateResumeAsync(UpdateResumeRequestDTO updateResumeRequestDto, string token);
 }
