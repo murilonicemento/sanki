b98391ef3ebe2d772aa267d6cc529903923f0d2e
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-05 22:47:50 -0300

    feat: add delete resume method

--- Sanki.Services.Contracts/IResumeService.cs
+++ Sanki.Services.Contracts/IResumeService.cs
@@ -7,4 +7,5 @@ public interface IResumeService
     public Task<List<ResumeResponseDTO>> GetResumesByUserAsync(string token);
     public Task AddResumeAsync(AddResumeRequestDTO addResumeRequestDto, string token);
     public Task<ResumeResponseDTO> UpdateResumeAsync(UpdateResumeRequestDTO updateResumeRequestDto, string token);
+    public Task DeleteResumeAsync(Guid id, string token);
 }
