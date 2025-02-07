6554f0c3eda0f3c42f5d8eb45e40c9fc6a15615c
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-06 23:24:43 -0300

    tests: create test for add, update and delete feature

--- Sanki.Tests/ResumeServiceTest.cs
+++ Sanki.Tests/ResumeServiceTest.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using Sanki.Entities;
 using Sanki.Persistence;
 using Sanki.Services;
+using Sanki.Services.Contracts.DTO;
 
 namespace Sanki.Tests;
 
@@ -56,8 +57,8 @@ public class ResumeServiceTest
     public async Task GetResumesByUserAsync_NotAuthorizedUser_ShouldThrowUnauthorizedAccessException()
     {
         var user = _fixture.Build<User>()
-            .With(user => user.Resumes, new List<Resume>())
             .With(user => user.Email, string.Empty)
+            .With(user => user.Resumes, new List<Resume>())
             .Create();
         var loginUserResponseDto = _jwtService.GenerateJwt(user);
 
@@ -65,6 +66,229 @@ public class ResumeServiceTest
         {
             await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
         });
+    }
+
+    [Fact]
+    public async Task GetResumesByUserAsync_UserWithEmptyResumes_ShouldBeValid()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+
+        var resumes = await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
+
+        Assert.Empty(resumes);
+    }
+
+    [Fact]
+    public async Task GetResumesByUserAsync_UserWithResumes_ShouldBeValid()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var addResumeDto = _fixture.Build<AddResumeRequestDTO>().Create();
+
+        await _resumeService.AddResumeAsync(addResumeDto, loginUserResponseDto.Token);
+
+        var resumes = await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
+
+        Assert.NotEmpty(resumes);
+    }
+
+    #endregion
+
+    #region AddResumeAsync
+
+    [Fact]
+    public async Task AddResumeAsync_InvalidToken_ShouldThrowSecurityTokenException()
+    {
+        const string token = "invalid-token";
+        var addResumeDto = _fixture.Build<AddResumeRequestDTO>().Create();
+
+        await Assert.ThrowsAsync<SecurityTokenException>(async () =>
+        {
+            await _resumeService.AddResumeAsync(addResumeDto, token);
+        });
+    }
+
+    [Fact]
+    public async Task AddResumeAsync_NotAuthorizedUser_ShouldThrowUnauthorizedAccessException()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Id, Guid.Empty)
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var addResumeDto = _fixture.Build<AddResumeRequestDTO>().Create();
+
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(async () =>
+        {
+            await _resumeService.AddResumeAsync(addResumeDto, loginUserResponseDto.Token);
+        });
+    }
+
+    [Fact]
+    public async Task AddResumeAsync_ShouldBeValid()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var addResumeDto = _fixture.Build<AddResumeRequestDTO>().Create();
+
+        await _resumeService.AddResumeAsync(addResumeDto, loginUserResponseDto.Token);
+        var resumes = await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
+
+        Assert.NotEmpty(resumes);
+    }
+
+    #endregion
+
+    #region UpdateResumeAsync
+
+    [Fact]
+    public async Task UpdateResumeAsync_InvalidToken_ShouldThrowSecurityTokenException()
+    {
+        const string token = "invalid-token";
+        var updateResumeRequestDto = _fixture.Build<UpdateResumeRequestDTO>().Create();
+
+        await Assert.ThrowsAsync<SecurityTokenException>(async () =>
+        {
+            await _resumeService.UpdateResumeAsync(updateResumeRequestDto, token);
+        });
+    }
+
+    [Fact]
+    public async Task UpdateResumeAsync_NotAuthorizedUser_ShouldThrowUnauthorizedAccessException()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Email, string.Empty)
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var updateResumeRequestDto = _fixture.Build<UpdateResumeRequestDTO>().Create();
+
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(async () =>
+        {
+            await _resumeService.UpdateResumeAsync(updateResumeRequestDto, loginUserResponseDto.Token);
+        });
+    }
+
+    [Fact]
+    public async Task UpdateResumeAsync_NotFoundResumeForUser_ShouldKeyNotFoundException()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var updateResumeRequestDto = _fixture.Build<UpdateResumeRequestDTO>().Create();
+
+        updateResumeRequestDto.Id = Guid.NewGuid();
+
+        await Assert.ThrowsAsync<KeyNotFoundException>(async () =>
+        {
+            await _resumeService.UpdateResumeAsync(updateResumeRequestDto, loginUserResponseDto.Token);
+        });
+    }
+
+    [Fact]
+    public async Task UpdateResumeAsync_ShouldBeValid()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var updateResumeRequestDto = _fixture.Build<UpdateResumeRequestDTO>().Create();
+        var updateResumeExpected = await _resumeService
+            .UpdateResumeAsync(updateResumeRequestDto, loginUserResponseDto.Token);
+        var resume = await _sankiContext.Resumes.FirstOrDefaultAsync(resume => resume.Id == updateResumeExpected.Id);
+        var updateResumeResult = new ResumeResponseDTO
+        {
+            Id = resume.Id,
+            Title = resume.Title,
+            Content = resume.Content
+        };
+
+        Assert.Equal(updateResumeResult, updateResumeExpected);
+    }
+
+    #endregion
+
+    #region DeleteResumeAsync
+
+    [Fact]
+    public async Task DeleteResumeAsync_InvalidToken_ShouldThrowSecurityTokenException()
+    {
+        const string token = "invalid-token";
+        var id = Guid.NewGuid();
+
+        await Assert.ThrowsAsync<SecurityTokenException>(async () =>
+        {
+            await _resumeService.DeleteResumeAsync(id, token);
+        });
+    }
+
+    [Fact]
+    public async Task DeleteResumeAsync_NotAuthorizedUser_ShouldThrowUnauthorizedAccessException()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Email, string.Empty)
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var id = Guid.NewGuid();
+
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(async () =>
+        {
+            await _resumeService.DeleteResumeAsync(id, loginUserResponseDto.Token);
+        });
+    }
+
+    [Fact]
+    public async Task DeleteResumeAsync_ResumeAlreadyDeleted_ShouldThrowKeyNotFoundException()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Email, string.Empty)
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var addResume = _fixture.Build<AddResumeRequestDTO>().Create();
+
+        await _resumeService.AddResumeAsync(addResume, loginUserResponseDto.Token);
+
+        var resume = await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
+        var id = resume[0].Id;
+
+        await _resumeService.DeleteResumeAsync(id, loginUserResponseDto.Token);
+
+        await Assert.ThrowsAsync<KeyNotFoundException>(async () =>
+        {
+            await _resumeService.DeleteResumeAsync(id, loginUserResponseDto.Token);
+        });
+    }
+
+    [Fact]
+    public async Task DeleteResumeAsync_ShouldBeValid()
+    {
+        var user = _fixture.Build<User>()
+            .With(user => user.Email, string.Empty)
+            .With(user => user.Resumes, new List<Resume>())
+            .Create();
+        var loginUserResponseDto = _jwtService.GenerateJwt(user);
+        var addResume = _fixture.Build<AddResumeRequestDTO>().Create();
+
+        await _resumeService.AddResumeAsync(addResume, loginUserResponseDto.Token);
+
+        var resume = await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
+        var id = resume[0].Id;
+
+        await _resumeService.DeleteResumeAsync(id, loginUserResponseDto.Token);
+
+        var allResumes = await _resumeService.GetResumesByUserAsync(loginUserResponseDto.Token);
+
+        Assert.Empty(allResumes);
     }
 
     #endregion
