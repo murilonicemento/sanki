6952b52e07f6c6d4fad66d5546465558ea6f05df
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-12 00:06:00 -0300

    feat(interface): add GenerateFlashcard method

--- Sanki.Services.Contracts/IFlashcardService.cs
+++ Sanki.Services.Contracts/IFlashcardService.cs
@@ -5,5 +5,6 @@ namespace Sanki.Services.Contracts;
 public interface IFlashcardService
 {
     public Task<List<FlashcardResponseDTO>> GetFlashcardsByUserAsync(string token);
-    public Task<List<FlashcardResponseDTO>> GenerateFlashcards(string token);
+
+    public Task GenerateFlashcardsAsync(Guid resumeId, string token);
 }
