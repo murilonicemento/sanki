09b53577c446323bc135c1db8d36b06e3d532c0e
murilonicemento <murilo_nascimento22@outlook.com>
2025-02-12 00:07:36 -0300

    chore(model): add gemini response model class

---
+++ Sanki.Services.Contracts/GeminiResponse.cs
@@ -0,0 +1,21 @@
+namespace Sanki.Services.Contracts;
+
+public class GeminiResponse
+{
+    public List<Candidate> Candidates { get; set; }
+}
+
+public class Candidate
+{
+    public Content Content { get; set; }
+}
+
+public class Content
+{
+    public List<Part> Parts { get; set; }
+}
+
+public class Part
+{
+    public string Text { get; set; }
+}
